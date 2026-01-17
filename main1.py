5bbc6e1b02b5f44c8b8383370c86cbc9a4fd0aef
admin <kamilaakynbaeva@mail.ru>
2026-01-17 18:24:01 +0600

    first

---
+++ main1.py
@@ -0,0 +1,72 @@
+import flet as ft
+from datetime import datetime
+
+def main(page: ft.Page):
+    page.title = 'Мое первое приложение!'
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    text_hello = ft.Text(value='Hello world')
+
+    greeting_history = []
+    history_text = ft.Text('История приветствий:')
+
+    def text_name(_):
+        name = name_input.value.strip()
+        if name:
+            now=datetime.now().strftime('%Y:%m:%d-%h:%m:%s')
+            text_hello.color = None
+            text_hello.value = f'Hello {name}'
+            name_input.value = ""
+
+            greeting_history.append(name)
+            history_text.value = 'История приветствий:\n' + '\n'.join(greeting_history)
+        else:
+            text_hello.value = "Введите имя!"
+            text_hello.color = ft.Colors.RED
+        page.update()
+
+    elevated_button = ft.ElevatedButton(
+        'send',
+        on_click=text_name,
+        icon=ft.Icons.SEARCH
+    )
+
+    name_input = ft.TextField(
+        label='Введите что-нибудь',
+        on_submit=text_name,
+        expand=True
+    )
+
+    def thememode(_):
+        page.theme_mode = (
+            ft.ThemeMode.LIGHT
+            if page.theme_mode == ft.ThemeMode.DARK
+            else ft.ThemeMode.DARK
+        )
+        page.update()
+
+    thememode_button = ft.IconButton(
+        icon=ft.Icons.BRIGHTNESS_7,
+        on_click=thememode
+    )
+
+    def clear_history(_):
+        greeting_history.clear()
+        history_text.value = 'История приветствий:'
+        page.update()
+
+    clear_button = ft.IconButton(
+        icon=ft.Icons.DELETE,
+        on_click=clear_history
+    )
+
+    main_object = ft.Row([
+        name_input,
+        elevated_button,
+        thememode_button,
+        clear_button
+    ])
+
+    page.add(text_hello, main_object, history_text)
+
+ft.app(target=main)
